41b8c78da6c4520aaa090674265320160a9b45b4
OusmaneMballo <ousmanemballo576@gmail.com>
2020-06-09 04:15:58 +0000

    Validation et Fin TP

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ var Upbtn=document.getElementById("Upbtn");
 var downbtn=document.getElementById("downbtn");
 var rotateValue=cicle.style.transform;
 var rotateSum;
+
 Upbtn.onclick=function(){
     rotateSum=rotateValue + "rotate(-90deg)";
+    cicle.style.transform=rotateSum;
+    rotateValue=rotateSum;
+
+}
+
+downbtn.onclick=function(){
+    rotateSum=rotateValue + "rotate(90deg)";
     cicle.style.transform=rotateSum;
     rotateValue=rotateSum;
 
